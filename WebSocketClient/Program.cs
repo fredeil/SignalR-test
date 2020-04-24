df7d7e0169b4daaa0a25d43f2888474dd3fd1770
fredeil <fredrik.eilertsen@gmail.com>
2020-04-24 11:20:31 +0200

    Initial commit

---
+++ WebSocketClient/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+using System.Linq;
+using System.Buffers;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace WebSocketClient
+{
+    class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            var connections = new List<HubConnection>();
+            for (int i = 0; i <= 100; i++)
+            {
+                var connection = new HubConnectionBuilder()
+                    .WithUrl("http://localhost:18000/fredrik")
+                    .WithAutomaticReconnect()
+                    .Build();
+
+                connections.Add(connection);
+
+               // await connection.StartAsync();
+            }
+
+            await Task.WhenAll(from c in connections
+                               select c.StartAsync());
+
+            await Task.WhenAll(from c in connections
+                               select StreamingEcho(c));
+
+            return 0;
+        }
+
+        public static async Task StreamingEcho(HubConnection connection)
+        {
+            var channel = Channel.CreateUnbounded<string>();
+
+            _ = Task.Run(async () =>
+            {
+                for (var i = 0; i < 5; i++)
+                {
+                    await channel.Writer.WriteAsync(RandomString(4 * 1024));
+                }
+            });
+
+            var outputs = await connection.StreamAsChannelAsync<string>("StreamEcho", channel.Reader);
+
+            while (await outputs.WaitToReadAsync())
+            {
+                while (outputs.TryRead(out var item))
+                {
+                    Console.WriteLine($"Recv: '{item}'.");
+                }
+            }
+        }
+
+
+        static string RandomString(int length)
+        {
+            const string valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            var res = new StringBuilder();
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                byte[] uintBuffer = new byte[sizeof(uint)];
+
+                while (length-- > 0)
+                {
+                    rng.GetBytes(uintBuffer);
+                    uint num = BitConverter.ToUInt32(uintBuffer, 0);
+                    res.Append(valid[(int)(num % (uint)valid.Length)]);
+                }
+            }
+
+            return res.ToString();
+        }
+    }
+}
---
+++ WebSocketServer/Startup.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WebSocketTest
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR(options =>
+            {
+                // Faster pings for testing
+                options.KeepAliveInterval = TimeSpan.FromSeconds(5);
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+            app.UseEndpoints(endpoints => 
+            {
+                endpoints.MapHub<Chubby>("/fredrik");
+            });
+        }
+    }
+
+    public class Chubby : Hub
+    {
+        static int numConnections = 0;
+
+        public override Task OnConnectedAsync() 
+        {
+            Interlocked.Increment(ref numConnections);
+            Console.WriteLine($"New connection({numConnections}): "+ Context.ConnectionId);
+            return Task.CompletedTask;
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            Interlocked.Decrement(ref numConnections);
+            Console.WriteLine("Disconnected: "+ Context.ConnectionId);
+            return Task.CompletedTask;
+        }
+
+        public ChannelReader<string> StreamEcho(ChannelReader<string> source)
+        {
+            var output = Channel.CreateUnbounded<string>();
+
+            _ = Task.Run(async () =>
+            {
+                while (await source.WaitToReadAsync())
+                {
+                    while (source.TryRead(out var item))
+                    {
+                        await output.Writer.WriteAsync($"[{Context.ConnectionId.Substring(0, 5)}]: " + item);
+                    }
+                }
+
+                output.Writer.Complete();
+            });
+
+            return output.Reader;
+        }
+    }
+}
